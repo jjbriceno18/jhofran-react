791bbdb429d1213e84fbc5b9c10dc2430ad3a007
jjbriceno18 <jjbriceno18@gmail.com>
2021-04-22 10:53:20 -0500

    update 04

--- projectos/basico/src/App.js
+++ projectos/basico/src/App.js
@@ -1,9 +1,19 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Producto from './components/Producto';
 
 function App() {
+
+
+  // crear listado de productos
+  const [ productos, setProductos ] = useState([
+    { id: 1, nombre: 'Camisa React', precio: 50 },
+    { id: 2, nombre: 'Camisa Angula', precio: 20 },
+    { id: 3, nombre: 'Camisa Vues', precio: 30 },
+    { id: 4, nombre: 'Camisa Node', precio: 40 },
+  ])
 
   let titulo = 'Tienda Virtual'
   // Obtener fecha
@@ -14,6 +24,14 @@ function App() {
       <Header
         titulo={titulo}
       />
+
+      <h1>Lista de Productos</h1>
+      {productos.map(producto => (
+        <Producto
+          key={producto.id}
+          producto={producto}
+        />
+      ))}
 
       <Footer
         fecha={fecha}
