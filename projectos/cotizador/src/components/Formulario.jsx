86e4702388c73fe43d8f17e036e1ff1c59ba3132
jjbriceno18 <jjbriceno18@gmail.com>
2021-05-07 11:44:47 -0500

    update 14

--- projectos/cotizador/src/components/Formulario.jsx
+++ projectos/cotizador/src/components/Formulario.jsx
@@ -1,20 +1,40 @@
 import React from 'react';
+import styled from '@emotion/styled'
+
+const Campo = styled.div`
+    display: flex;
+    margin-bottom: 1rem;
+    align-items: center;
+`
+const Label = styled.label`
+    flex: 0 0 100px;
+`
+const Select = styled.select`
+    display: block;
+    width: 100%;
+    padding: 1rem;
+    border: 1px solid #e1e1e1;
+    -webkit-appearance: none;
+`
+const InputRadio = styled.input`
+    margin: 0 1rem;
+`
 
 const Formulario = () => {
     return (
         <form>
-            <div>
-                <label>Marca</label>
-                <select>
+            <Campo>
+                <Label>Marca</Label>
+                <Select>
                     <option value="">-- Seleccione --</option>
                     <option value="americana">Americana</option>
                     <option value="europea">Europea</option>
                     <option value="asiatica">Asiatica</option>
-                </select>
-            </div>
-            <div>
-                <label>Año</label>
-                <select>
+                </Select>
+            </Campo>
+            <Campo>
+                <Label>Año</Label>
+                <Select>
                 <option value="">-- Seleccione --</option>
                 <option value="2021">2021</option>
                 <option value="2020">2020</option>
@@ -26,21 +46,21 @@ const Formulario = () => {
                 <option value="2014">2014</option>
                 <option value="2013">2013</option>
                 <option value="2012">2012</option>
-                </select>
-            </div>
-            <div>
-                <label>Plan</label>
-                <input
+                </Select>
+            </Campo>
+            <Campo>
+                <Label>Plan</Label>
+                <InputRadio
                     type="radio"
                     name="plan"
                     value="basico"
                 /> Basico
-                <input
+                <InputRadio
                     type="radio"
                     name="plan"
                     value="completo"
                 /> Completo
-            </div>
+            </Campo>
 
             <button type="button">Cotizar</button>
         </form>
