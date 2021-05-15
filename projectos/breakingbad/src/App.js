9989a2b04b5e0fbc6d1e5adca93ed5532a4556b9
Jhofran Briceño <jjbriceno18@gmail.com>
2021-05-15 06:45:41 -0500

    update 18

--- projectos/breakingbad/src/App.js
+++ projectos/breakingbad/src/App.js
@@ -1,4 +1,7 @@
+import { useState, useEffect } from 'react'
 import styled from '@emotion/styled'
+import Frase from './components/Frase'
+
 
 const Contenedor = styled.div`
   display: flex;
@@ -15,18 +18,35 @@ const Boton = styled.button`
   margin-top: 3rem;
   padding: 1rem 3rem;font-size: 2rem;
   border: 2px solid black;
+  transition: background-size .8s ease;
+
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
 `
 
 function App() {
 
+  // state de frase
+  const [frase, guardarFrase] = useState({})
+
   const consultarAPI = async () => {
     const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes')
     const frase = await api.json()
-    console.log(frase[0])
+    guardarFrase(frase[0])
   }
+
+  // cargar frase
+  useEffect( () => {
+    consultarAPI()
+  }, [])
 
   return (
     <Contenedor className="App">
+      <Frase
+        frase={frase}
+      />
       <Boton
         onClick={consultarAPI}
       >Obtener Frase</Boton>
